69853012af25b80d20191240ad7afaaa9cdb2553
Jenitza Ruiz <jruiz5@seattleu.edu>
2024-10-07 20:09:43 -0700

    I wrote we are team 4 in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     ///<summary>
-    ///Jenitza Ruiz
+    ///weare team 4s
     ///</summary>
     public class IndexModel : PageModel
     {
