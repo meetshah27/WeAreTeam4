41bf657a67d74ee3920c83f76bf402612d214a09
Jenitza-182 <jenitza.ruizc@gmail.com>
2024-10-28 22:16:03 -0700

    Created Update page Back-end in Update.cshtml.cs

---
+++ src/Pages/Product/Update.cshtml.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class UpdateModel : PageModel
+{
+    // Data middle-tier
+    public JsonFileProductService ProductService { get; }
+
+    /// <summary> Default Constructor </summary>
+    public UpdateModel(JsonFileProductService productService)
+    {
+        ProductService = productService;
+    }
+
+    // The data to show, bind to it for the post
+    [BindProperty]
+    public ProductModel Product { get; set; }
+
+    /// <summary> REST Get request Loads the Data </summary>
+    public void OnGet(string id)
+    {
+        Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+    }
+    
+    /// <summary> Post the model back to the page The model is in the class variable </summary>
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        ProductService.UpdateData(Product);
+
+        return RedirectToPage("./Index");
+    }
+}
