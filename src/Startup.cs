a2f312011b152b0d4123a57ea3d15aa3cb48b9e0
meetshah27 <meet.shah.5458498@gmail.com>
2024-11-02 22:22:05 -0700

    Added ExceptionHandler in Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -33,13 +33,13 @@ namespace ContosoCrafts.WebSite
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                return;
             }
-                        
+            else
+            {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            
+            }            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
