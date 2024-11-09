453633b02d3de0344fcc0f8074b076d1d6524808
Jenitza-182 <jenitza.ruizc@gmail.com>
2024-11-08 22:25:46 -0800

    Ensured that spaces between lines were consistent and purposeful in Delete.cshtml.cs

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -1,11 +1,13 @@
-using ContosoCrafts.WebSite.Models; // Importing models for product representation
 using ContosoCrafts.WebSite.Services;  // Importing services for data handling
+
 using Microsoft.AspNetCore.Mvc;        // Importing ASP.NET MVC components
+
 using Microsoft.AspNetCore.Mvc.RazorPages;  // Importing Razor Pages components
 
 
 public class DeleteModel : PageModel
 {
+
     // Data middle-tier service to interact with product data
     public JsonFileProductService ProductService { get; }
 
@@ -13,7 +15,9 @@ public class DeleteModel : PageModel
     /// with a specified product service. </summary>
     public DeleteModel(JsonFileProductService productService)
     {
+
         ProductService = productService;
+
     }
 
     // The data model for a product, bound for form submission
@@ -24,20 +28,29 @@ public class DeleteModel : PageModel
     /// <param name="id"></param>
     public void OnGet(string id)
     {
+
         // Fetch all product data and find the first product that matches the given ID
         ProductId = id;
+
     }
 
     /// <summary> Delete the product with the saved id </summary>
     public IActionResult OnPost(string id)
-    {      // Check if the model state is valid; if not, return to the same page
+    {
+        // Check if the model state is valid; if not, return to the same page
         if (ModelState.IsValid == false)
         {
+
             return Page();
+
         }
+
         // Delete the product with the given id
         ProductService.DeleteData(id);
+
         // Redirect to the Index page after deletion
         return RedirectToPage("./Index");
+
     }
+
 }
