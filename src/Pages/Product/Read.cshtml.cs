fbf39bdf79b604739d8bd4b5d9ce1c4e1c57aa74
meetshah27 <meet.shah.5458498@gmail.com>
2024-11-14 22:08:18 -0800

    Update the OnGet method for test case of opening all the CRUD operation in diffferent windows to work properly in Read.cshtml.cs

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -1,5 +1,6 @@
 using ContosoCrafts.WebSite.Models;  // Importing models for product representation
 using ContosoCrafts.WebSite.Services;  // Importing services for data handling
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;  // Importing Razor Pages components
 using System.Linq;                         // Importing LINQ for querying collections
 
@@ -43,13 +44,20 @@ public class ReadModel : PageModel
 
     /// <param name="id"></param>
 
-    public void OnGet(string id)
-
+    public IActionResult OnGet(string id)
     {
         // Fetch all product data and find the first product that matches the given ID
         Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
 
+        // Check if the product or its title is null; if so, redirect to the Index page
+        if (Product == null || string.IsNullOrEmpty(Product.Title))
+        {
+            return RedirectToPage("./Index"); // Redirect to the Index page
+        }
+
+        return Page(); // Otherwise, load the page normally
     }
+
 
 }
 
