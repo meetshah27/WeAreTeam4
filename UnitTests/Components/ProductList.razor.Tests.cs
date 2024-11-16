367bb2229368ca45c6c50a6b666ce89224706473
LanceFyre99 <lance.fyre@live.com>
2024-11-16 14:18:59 -0800

    Added test for submitting a star rating in ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -49,7 +49,7 @@ namespace UnitTests.Components
 
             var page = RenderComponent<ProductList>();
 
-            // Find the Buttons (more info)
+            // Find product blocks
             var buttonList = page.FindAll("A");
 
             // Find the one that matches the ID looking for, for clicking
@@ -64,6 +64,44 @@ namespace UnitTests.Components
             // Assert
             Assert.That(true, Is.EqualTo(pageMarkup.Contains("An online learning platform partnering with universities and organizations to offer courses &amp; specializations. Users can access high-quality educational content across various fields.")));
             
+        }
+
+        [Test]
+        public void SubmitRating_Valid_ID_Click_Unstarred_Should_Increment_Count_And_Check_Star()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "jenlooper-lightshow_MoreInfo";
+
+            var page = RenderComponent<ProductList>();
+
+            // Find product blocks
+            var buttonList = page.FindAll("A");
+
+            // Find the one that matches the ID looking for and click it
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+            button.Click();
+
+            // Get the markup page post-click
+            var prePageMarkup = page.Markup;
+
+            // Get star buttons
+            var starButtonList = page.FindAll("span");
+
+            // Get first star button from the list
+            var starButton = starButtonList.First(m => !string.IsNullOrEmpty(m.ClassName) && m.ClassName.Contains("fa fa-star"));
+
+            // Act
+            // Click the star button
+            starButton.Click();
+
+            // Get the markup page post-star-click
+            var postPageMarkup = page.Markup;
+
+            // Assert
+            // Confirm that the record had no votes to start, and 1 vote after
+            Assert.That(prePageMarkup.Contains("Be the first to vote!"), Is.EqualTo(true));
+            Assert.That(postPageMarkup.Contains("1 Vote"), Is.EqualTo(true));
         }
     }
 }
