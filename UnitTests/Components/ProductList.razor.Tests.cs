1e2d938d6457c1d596c7d9f78afb6051b93e3cd3
LanceFyre99 <lance.fyre@live.com>
2024-11-16 13:47:19 -0800

    Added test for clicking a product block in ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -5,6 +5,8 @@ using NUnit.Framework;
 using ContosoCrafts.WebSite.Components;
 
 using ContosoCrafts.WebSite.Services;
+using Bunit;
+using System.Linq;
 
 namespace UnitTests.Components
 {
@@ -38,6 +40,30 @@ namespace UnitTests.Components
 
         }
 
-    }
+        [Test]
+        public void SelectProduct_Valid_ID_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "jenlooper-light_MoreInfo";
 
+            var page = RenderComponent<ProductList>();
+
+            // Find the Buttons (more info)
+            var buttonList = page.FindAll("A");
+
+            // Find the one that matches the ID looking for, for clicking
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+
+            // Act
+            button.Click();
+
+            // Get the markup page for the assert
+            var pageMarkup = page.Markup;
+
+            // Assert
+            Assert.That(true, Is.EqualTo(pageMarkup.Contains("An online learning platform partnering with universities and organizations to offer courses &amp; specializations. Users can access high-quality educational content across various fields.")));
+            
+        }
+    }
 }
