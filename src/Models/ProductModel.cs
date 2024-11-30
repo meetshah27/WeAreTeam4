4f4b092825530d4db850b8399bff0c7cdc1bd778
LanceFyre99 <lance.fyre@live.com>
2024-11-30 12:00:21 -0800

    Added enum value for representing product type to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -6,6 +6,52 @@ using System.Text.Json.Serialization;
 namespace ContosoCrafts.WebSite.Models
 {
     /// <summary>
+    /// Table of enums values for designating the type of product
+    /// </summary>
+    public enum ProductTypeEnum
+    {
+        undefined = 0,
+        education = 1,
+        finance = 3,
+        social_media = 5,
+        shopping = 7,
+        transport = 9,
+        food_delivery = 11,
+        software_development = 13,
+        other = 12345,
+    }
+
+    /// <summary>
+    /// Extensions of ProductTypeEnum to allow 
+    /// </summary>
+    public static class ProductTypeEnumExtensions
+    {
+        /// <summary>
+        /// Returns string format of enum type for front-facing representation
+        /// </summary>
+        /// <param name="data">The enum being referenced</param>
+        /// <returns>String representation of enum type if enum is valid & defined, blank string otherwise</returns>
+        public static string DisplayName(this ProductTypeEnum data)
+        {
+            return data switch
+            {
+                ProductTypeEnum.education => "Educational",
+                ProductTypeEnum.finance => "Banking & Finance",
+                ProductTypeEnum.social_media => "Social Media",
+                ProductTypeEnum.shopping => "Shopping",
+                ProductTypeEnum.transport => "Transport & Travel",
+                ProductTypeEnum.food_delivery => "Food Delivery",
+                ProductTypeEnum.software_development => "Software Development",
+
+
+                // Default, Unknown
+                _ => "",
+            };
+        }
+    }
+
+
+    /// <summary>
     /// Represents a product model with relevant details such as ID, maker, title, description, ratings, and counters.
     /// </summary>
     public class ProductModel
@@ -61,5 +107,10 @@ namespace ContosoCrafts.WebSite.Models
         /// Gets or sets the counter representing the number of URL accesses for the product.
         /// </summary>
         public int UrlCounter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the enum representing what type of service the product provides
+        /// </summary>
+        public ProductTypeEnum ProductType { get; set; }
     }
 }
